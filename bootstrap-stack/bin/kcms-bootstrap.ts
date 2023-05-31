fcc744142f85d8faeba87a685c2f38bd2ef44cd6
David Nielsen <dn@keycore.dk>
2023-05-31 13:33:29 +0200

    removed hardcoded account id

--- bootstrap-stack/bin/kcms-bootstrap.ts
+++ bootstrap-stack/bin/kcms-bootstrap.ts
@@ -2,8 +2,8 @@
 import * as cdk from 'aws-cdk-lib';
 import "source-map-support/register";
 import { KCMSBootstrapStack } from '../lib/stacks/bootstrap-stack';
-import { getContext, stackProps } from '../context';
+import { getContext} from '../context';
 
 const app = new cdk.App();
 const context = getContext(app);
-new KCMSBootstrapStack(app, 'KCMSBootstrapStack', stackProps("953913856690", "eu-west-1") , context);
+new KCMSBootstrapStack(app, 'KCMSBootstrapStack', {} , context);
--- scripts/cloudshell.sh
+++ scripts/cloudshell.sh
@@ -2,19 +2,13 @@
 
 set -e
 
-mkdir "${PWD}/.npm-packages"
-
-sudo npm install --user npm@latest yarn
+sudo npm install --user npm@latest
 npm config set cache $PWD/npm-cache
-yarn config set cache-folder $PWD/yarn-cache
-yarn install
-yarn synth
+npm install
+
 aws_account_id=$(aws sts get-caller-identity --query Account --output text)
 aws_region=$AWS_DEFAULT_REGION
 
 echo "Bootstrapping $aws_account_id/$aws_region"
-yarn cdk:bootstrap $aws_account_id/$aws_region
-yarn kcms:bootstrap
-
-cd $HOME
-sudo rm -rf /home/bootstrap-stack
+npm run cdk:bootstrap $aws_account_id/$aws_region
+npm run kcms:bootstrap
